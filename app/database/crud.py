859cc3163a7cdc2de76a257c7fe2e85988539748
hiiggs <yurasik@gmail.com>
2023-11-06 18:06:35 +0300

    add: Get all todos, Get one todo, Put todo, Delete todo

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import Session
 
-
 from . import models, schemas
+
+
+def get_todo(db: Session, todo_id: int):
+    return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+
+
+def get_todos(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Todo).offset(skip).limit(limit).all()
 
 
 def create_todo(db: Session, todo: schemas.TodoCreate):
@@ -10,3 +17,29 @@ def create_todo(db: Session, todo: schemas.TodoCreate):
     db.commit()
     db.refresh(db_todo)
     return db_todo
+
+
+def update_todo(db: Session,
+                todo_id: int,
+                new_title: str | None = None,
+                new_description: str | None = None,
+                new_status: bool = False, ):
+    db_todo: models.Todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db_todo.title = new_title
+    db_todo.description = new_description
+    db_todo.is_completed = new_status
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+def delete_todo(db: Session, todo_id: int):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo:
+        db.delete(todo)
+        db.commit()
+        return True
+    else:
+        return False
+
--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,17 +1,60 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from ..database.crud import create_todo
+from ..database.crud import create_todo, delete_todo, get_todo, get_todos, update_todo
 from ..database.schemas import Todo, TodoCreate
 from ..dependencies import get_db
 
-
 router = APIRouter(
-    prefix="/items",
-    tags=["items"]
+    prefix="/todo",
+    tags=["todos"]
 )
+
+
+@router.get("/", response_model=list[Todo])
+async def get_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    todos = get_todos(db=db, skip=skip, limit=limit)
+    if not todos:
+        raise HTTPException(status_code=400, detail="Todos doesn't exist")
+    return todos
+
+
+@router.get("/{todo_id}", response_model=Todo)
+async def get_item(todo_id: int, db: Session = Depends(get_db)):
+    todo = get_todo(db=db, todo_id=todo_id)
+    if todo is None:
+        raise HTTPException(status_code=400, detail="Todo with this ID don't exist")
+    return todo
 
 
 @router.post("/create", response_model=Todo)
 async def create_item(data: TodoCreate, db: Session = Depends(get_db)):
     return create_todo(db=db, todo=data)
+
+
+@router.put("/{todo_id}", response_model=Todo)
+async def update_item(
+        todo_id: int,
+        new_title: str | None = None,
+        new_description: str | None = None,
+        new_status: bool = False,
+        db: Session = Depends(get_db)
+):
+    todo = update_todo(
+        db=db,
+        todo_id=todo_id,
+        new_title=new_title,
+        new_description=new_description,
+        new_status=new_status,
+    )
+    if todo is None:
+        raise HTTPException(status_code=400, detail="Todo with this ID don't exist")
+    return todo
+
+
+@router.delete("{todo_id}")
+async def delete_item(todo_id: int, db: Session = Depends(get_db)):
+    status_delete = delete_todo(db=db, todo_id=todo_id)
+    if not status_delete:
+        raise HTTPException(status_code=400, detail="Todo with this ID don't exist")
+    return {"Delete was successfully"}
